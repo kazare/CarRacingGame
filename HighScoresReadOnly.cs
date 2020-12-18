4aa40b386f23355d2815519f67e1ca46f1e8f382
Kazare <12612446+kazare@users.noreply.github.com>
2020-12-18 13:30:48 -0500

    Add read only high score form

---
+++ HighScoresReadOnly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CarRacingGame
+{
+    public partial class HighScoresReadOnly : Form
+    {
+        public HighScoresReadOnly()
+        {
+            InitializeComponent();
+        }
+
+        // Dispaly database contents to data grid view
+        private void HighScoresReadOnly_Load(object sender, EventArgs e)
+        {
+            HighScoreQuery query = new HighScoreQuery();
+            highScoresBindingSource.DataSource = query.ShowTableData();
+            dgv.DataSource = highScoresBindingSource;
+        }
+    }
+}
